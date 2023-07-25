f6b0d48eac7218e5dc4f94d1af858ab073ac598c
Enrico Ros <enrico.ros@gmail.com>
2023-07-24 19:11:07 -0700

    Halfway thought JSON purposes

--- src/data.ts
+++ src/data.ts
@@ -1,108 +1,114 @@
-import * as React from 'react';
+import { z } from 'zod';
 
-export type SystemPurposeId = 'Catalyst' | 'Custom' | 'Designer' | 'Developer' | 'Executive' | 'Generic' | 'Scientist';
+import customPersonas from '~/common/system-purposes.json';
+
+export type SystemPurposeId = string;
 
 export const defaultSystemPurposeId: SystemPurposeId = 'Generic';
 
-type SystemPurposeData = {
-  title: string;
-  description: string | React.JSX.Element;
-  systemMessage: string;
-  symbol: string;
-  examples?: string[];
-  highlighted?: boolean;
-}
+
+// Type definitions for both local and JSON validation purposes
+
+const personaSchema = z.object({
+  id: z.string(),
+  title: z.string(),
+  description: z.string(),
+  systemMessage: z.string().nullable(),
+  symbol: z.string(),
+  examples: z.array(z.string()).optional(),
+  highlighted: z.boolean().optional(),
+});
+
+const settingsSchema = z.object({
+  hiddenPurposeIDs: z.array(z.string()),
+});
+
+const systemPurposesSchema = z.object({
+  personas: z.array(personaSchema),
+  settings: settingsSchema,
+});
+
+type PersonaSchema = z.infer<typeof personaSchema>;
+type SettingsSchema = z.infer<typeof settingsSchema>;
 
 
-function validateSystemPurposes(jsonData: any): { [key in SystemPurposeId]: SystemPurposeData } | null {
-  if (typeof jsonData !== 'object' || jsonData === null) {
-    return null;
-  }
+// Default personas - overlayed with user customizations
 
-  for (const key in jsonData) {
-    if (!jsonData.hasOwnProperty(key)) {
-      continue;
-    }
-
-    const data = jsonData[key];
-    if (
-        typeof data.title !== 'string' ||
-        typeof data.description !== 'string' ||
-        typeof data.systemMessage !== 'string' ||
-        typeof data.symbol !== 'string' ||
-        (data.examples && !Array.isArray(data.examples)) ||
-        (data.highlighted && typeof data.highlighted !== 'boolean')
-    ) {
-      return null;
-    }
-  }
-
-  return jsonData;
-}
-
-let SystemPurposes: { [key in SystemPurposeId]: SystemPurposeData } = {
-  Developer: {
+let Personas: PersonaSchema[] = [
+  {
+    id: 'Developer',
     title: 'Developer',
     description: 'Helps you code',
     systemMessage: 'You are a sophisticated, accurate, and modern AI programming assistant', // skilled, detail-oriented
     symbol: '👩‍💻',
     examples: ['hello world in 10 languages', 'translate python to typescript', 'find and fix a bug in my code', 'add a mic feature to my NextJS app', 'automate tasks in React'],
   },
-  Scientist: {
+  {
+    id: 'Scientist',
     title: 'Scientist',
     description: 'Helps you write scientific papers',
     systemMessage: 'You are a scientist\'s assistant. You assist with drafting persuasive grants, conducting reviews, and any other support-related tasks with professionalism and logical explanation. You have a broad and in-depth concentration on biosciences, life sciences, medicine, psychiatry, and the mind. Write as a scientific Thought Leader: Inspiring innovation, guiding research, and fostering funding opportunities. Focus on evidence-based information, emphasize data analysis, and promote curiosity and open-mindedness',
     symbol: '🔬',
     examples: ['write a grant proposal on human AGI', 'review this PDF with an eye for detail', 'explain the basics of quantum mechanics', 'how do I set up a PCR reaction?', 'the role of dark matter in the universe'],
   },
-  Catalyst: {
+  {
+    id: 'Catalyst',
     title: 'Catalyst',
     description: 'Growth hacker with marketing superpowers 🚀',
     systemMessage: 'You are a marketing extraordinaire for a booming startup fusing creativity, data-smarts, and digital prowess to skyrocket growth & wow audiences. So fun. Much meme. 🚀🎯💡',
     symbol: '🚀',
     examples: ['blog post on AGI in 2024', 'add much emojis to this tweet', 'overcome procrastination!', 'how can I improve my communication skills?'],
   },
-  Executive: {
+  {
+    id: 'Executive',
     title: 'Executive',
     description: 'Helps you write business emails',
     systemMessage: 'You are an AI corporate assistant. You provide guidance on composing emails, drafting letters, offering suggestions for appropriate language and tone, and assist with editing. You are concise. ' +
-        'You explain your process step-by-step and concisely. If you believe more information is required to successfully accomplish a task, you will ask for the information (but without insisting).\n' +
-        'Knowledge cutoff: 2021-09\nCurrent date: {{Today}}',
+      'You explain your process step-by-step and concisely. If you believe more information is required to successfully accomplish a task, you will ask for the information (but without insisting).\n' +
+      'Knowledge cutoff: 2021-09\nCurrent date: {{Today}}',
     symbol: '👔',
     examples: ['draft a letter to the board', 'write a memo to the CEO', 'help me with a SWOT analysis', 'how do I team build?', 'improve decision-making'],
   },
-  Designer: {
+  {
+    id: 'Designer',
     title: 'Designer',
     description: 'Helps you design',
     systemMessage: 'You are an AI visual design assistant. You are expert in visual communication and aesthetics, creating stunning and persuasive SVG prototypes based on client requests. When asked to design or draw something, please work step by step detailing the concept, listing the constraints, setting the artistic guidelines in painstaking detail, after which please write the SVG code that implements your design.',
     symbol: '🖌️',
     examples: ['minimalist logo for a tech startup', 'infographic on climate change', 'suggest color schemes for a website'],
   },
-  Generic: {
+  {
+    id: 'Generic',
     title: 'Generic',
     description: 'Helps you think',
     systemMessage: 'You are ChatGPT, a large language model trained by OpenAI, based on the GPT-4 architecture.\nKnowledge cutoff: 2021-09\nCurrent date: {{Today}}',
     symbol: '🧠',
     examples: ['help me plan a trip to Japan', 'what is the meaning of life?', 'how do I get a job at OpenAI?', 'what are some healthy meal ideas?'],
   },
-  Custom: {
+  {
+    id: 'Custom',
     title: 'Custom',
     description: 'User-defined purpose',
     systemMessage: 'You are ChatGPT, a large language model trained by OpenAI, based on the GPT-4 architecture.\nCurrent date: {{Today}}',
     symbol: '✨',
   },
-};
+];
+//
+// let Settings: SettingsSchema = {
+//   hiddenPurposeIDs: [],
+// };
+// try {
+//   // load user customizations
+//
+//   // validate JSON with zod
+//   const { personas, settings } = systemPurposesSchema.parse(systemPurposesJson);
+//
+//   // merge (overlay) with default values
+//
+//
+// } catch (error) {
+//   console.error('Failed to load customization/system-purposes.json:', error);
+// }
 
-try {
-  const systemPurposesJson = require('../customization/system-purposes.json');
-  const validatedSystemPurposes = validateSystemPurposes(systemPurposesJson.Purposes);
-
-  if (validatedSystemPurposes) {
-    SystemPurposes = { ...SystemPurposes, ...validatedSystemPurposes };
-  } else {
-    console.error('Failed to validate systemPurposes.json: Invalid structure');
-  }
-} catch (error) {
-  console.error('Failed to load systemPurposes.json:', error);
-}
-export { SystemPurposes };
+// export { SystemPurposes };
+console.log(customPersonas)
